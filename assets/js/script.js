1c7dd2b4b2d0b590875b36414e0c345adf3fa3fe
ryanjolley12 <ryanswims@gmail.com>
2021-06-28 22:50:21 -0400

    API setups, input and search buttons

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+// LINES 1-500: BEN
+// LINES 501-1000: ANI
+// LINES 1001-1500: RYAN
+// LINES 1501-2000: SHANE
+// LINES 2001-2500: GAYE
+
+
+
+
+
+
+
+// RYAN'S CODE 
+
+// FETCH THE PRODUCE API (LOCAL FARMERS MARKETS)
+function getProduce() {
+    var searchProduce = document.querySelector("#searchProduce").value;
+
+    fetch('')
+}
+
+
+// FETCH THE RECIPE API 
+
+function getRecipe() {
+    var 
+}
