2a9117028a9dceb890678f6fee529bda0b20dd51
bengallagher <bgallag5@gmail.com>
2021-07-04 16:34:02 -0400

    two google maps display

--- assets/js/script.js
+++ assets/js/script.js
@@ -160,6 +160,28 @@ function getRecipe() {
     });
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //////////////////////////////////////ANI STORING ZIP CODE ////////////////////////////////////////////////////////
 if (window.localStorage) {
   var storeZip = document.getElementById("zipCode");
@@ -247,14 +269,6 @@ if (window.localStorage) {
 
 
 
-
-
-
-
-
-
-
-
 // "http://maps.google.com/?q=38.776991%2C%20-77.263568%20(%22Burke+%22)"
 // "http://maps.google.com/?q=Burke++Farmers+Market"
 // "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3109.543957590443!2d-77.30029918465263!3d38.797087779586235!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89b64e1b5983a451%3A0x311f6970ab8a1a77!2sBurke%20Farmers%20Market!5e0!3m2!1sen!2sus!4v1625148725193!5m2!1sen!2sus"
@@ -271,33 +285,22 @@ if (window.localStorage) {
 
 function findMarkets() {
   var zip = zipCode.value;
-  var markets = document.getElementById("market-display");
   var zipInput = document.getElementById("zipBox");
-  var title1 = document.getElementById("title-1");
-  var map1 = document.getElementById("map-1");
-  // var title2 = document.getElementById("title-2");
-  // var map2 = document.getElementById("map-2");
-  // var title3 = document.getElementById("title-3");
-  // var map3 = document.getElementById("map-3");
-  // var title4 = document.getElementById("title-4");
-  // var map4 = document.getElementById("map-4");
 
   zipInput.style.display = "none";
-  markets.style.display = "flex";
-
+//fetch local farmers markets from zipcode search
   fetch(
     "https://search.ams.usda.gov/farmersmarkets/v1/data.svc/zipSearch?zip=" +
       zip
   )
     .then(function (response) {
       return response.json();
-    })
+    })//grab id's of the two closest markets
     .then(function (response) {
       var market1 = response.results[0].id;
       var market2 = response.results[1].id;
-      var market3 = response.results[2].id;
-      var market4 = response.results[3].id;
-
+      console.log(market1);
+      //new fetches with the market id's
       fetch(
         "https://search.ams.usda.gov/farmersmarkets/v1/data.svc/mktDetail?id=" +
           market1
@@ -307,75 +310,57 @@ function findMarkets() {
         }) //capture google link and edit it to make it a better google maps search term (the link is originally given...
         //..as a long/lat location; so I have to clear the string of numbers and make sure the location has the words 'Farmers Market' in them)
         .then(function (response) {
-          // console.log(response.marketdetails.GoogleLink)
+          console.log(response);
           var link = response.marketdetails.GoogleLink;
           link = link.replace(/[^a-z+/.:?=]/gi, "").replace([".C."], "");
           if (!link.includes("Farmers")) {
             link = link.concat("+Farmers+Market");
           }
-          title1.textContent = response.marketdetails.Address;
-          map1.href = link;
-          showMaps(link);
-        });
-      fetch(
-        "https://search.ams.usda.gov/farmersmarkets/v1/data.svc/mktDetail?id=" +
-          market2
-      )
-        .then(function (response) {
-          return response.json();
-        })
-        .then(function (response) {
-          // console.log(response.marketdetails.GoogleLink)
-          var link2 = response.marketdtetails.GoogleLink;
-          link2 = link2.replace(/[^a-z+/.:?=]/gi, "").replace([".C."], "");
-          if (!link2.includes("Farmers")) {
-            link2 = link2.concat("+Farmers+Market");
-          }
-          title2.textContent = response.marketdetails.Address;
-          map2.href = link2;
-          showMaps(link2);
-        });
-      fetch(
-        "https://search.ams.usda.gov/farmersmarkets/v1/data.svc/mktDetail?id=" +
-          market3
-      )
-        .then(function (response) {
-          return response.json();
-        })
-        .then(function (response) {
-          // console.log(response.marketdetails.GoogleLink);
-          var link3 = response.marketdetails.GoogleLink;
-          link3 = link3.replace(/[^a-z+/.:?=]/gi, "").replace([".C."], "");
-          if (!link3.includes("Farmers")) {
-            link3 = link3.concat("+Farmers+Market");
-          }
-          title3.textContent = response.marketdetails.Address;
-          map3.href = link3;
-          showMaps(link3);
+          console.log(link);
+          fetch(
+            "https://search.ams.usda.gov/farmersmarkets/v1/data.svc/mktDetail?id=" +
+              market2
+          )
+            .then(function (response2) {
+              return response2.json();
+            })
+            .then(function (response2) {
+              console.log(response2);
+              var link2 = response2.marketdetails.GoogleLink;
+              link2 = link2.replace(/[^a-z+/.:?=]/gi, "").replace([".C."], "");
+              if (!link2.includes("Farmers")) {
+                link2 = link2.concat("+Farmers+Market");
+              }
+              console.log(link2);
+              showMaps(link, link2);
+            });
         });
     });
 }
 
 showMaps = function (link, link2) {
   console.log(link);
+  //split the USDA maps link and grab the second half(the search term), and insert it into a google maps embed link
   var market = link.split("q=");
   console.log(market[1]);
   var marketName = market[1];
   var embedLink =
     "https://www.google.com/maps/embed/v1/place?key=AIzaSyD6qU4Fdx74Tp9Z0lcCt26TIjLK8iC1uBk&q=" +
     marketName;
-
-  var map1 = document.getElementById("mapBox");
+//push it to the page
+  var map1 = document.getElementById("mapBox1");
   map1.setAttribute("src", embedLink);
 
-  //   var market = link2.split("q=");
-  //   var marketName = market[1];
-  //   var embedLink =
-  //     "https://www.google.com/maps/embed/v1/place?key=AIzaSyD6qU4Fdx74Tp9Z0lcCt26TIjLK8iC1uBk&q=" +
-  //     marketName;
-  //   var map2 = document.createElement("iframe")
-  //   map2.setAttribute({"src": embedLink, "width": "500", "height": "500", "style": "border:0", "allowfullscreen": "", "loading": "lazy", "class": "col-4"})
-  // mapContainer.appendChild(map2);
+  console.log(link2);
+  var market2 = link2.split("q=");
+  console.log(market2[1]);
+  var marketName2 = market2[1];
+  var embedLink2 =
+    "https://www.google.com/maps/embed/v1/place?key=AIzaSyD6qU4Fdx74Tp9Z0lcCt26TIjLK8iC1uBk&q=" +
+    marketName2;
+
+  var map2 = document.getElementById("mapBox2");
+  map2.setAttribute("src", embedLink2);
 };
 
 //---------------------Bens Code--------------------------------Bens Code-----------------------------------------------------------//
