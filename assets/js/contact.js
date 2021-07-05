1d79a67ee20172595de9d30d27fda5dbfb280239
ryanjolley12 <ryanswims@gmail.com>
2021-07-04 22:54:09 -0400

    added localstorage setup

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -3,6 +3,16 @@ var userEmail
 var userMessage
 var userRating 
 
+// WHEN USER CLICKS SAVE, THE TEXT VALUE IS OBTAINED
+$(".saveBtn").on("click", function () {
+    // OBTAIN TEXT VALUES FROM ROW CLASS AND ID 
+    var textValue = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+
+    // SET TO SAVE IN LOCALSTORAGE
+    localStorage.setItem(time, textValue);
+});
+
 // Set Name to localStorage
 
 $("name .description").val(localStorage.getItem(userName));
@@ -19,3 +29,16 @@ $("#message .description").val(localStorage.getItem(userMessage));
 // Set Rating to localStorage (?)
 
 $("#rating .description").val(localStorage.getItem(userRating));
+
+var saveTasks = function() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  };
+
+  // remove all tasks
+$("#remove-tasks").on("click", function() {
+    for (var key in tasks) {
+      tasks[key].length = 0;
+      $("#list-" + key).empty();
+    }
+    saveTasks();
+  });
