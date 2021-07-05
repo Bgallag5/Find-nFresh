3952e6a51631e810902e9df69f7057a2cf0e071b
ryanjolley12 <ryanswims@gmail.com>
2021-07-05 10:25:16 -0400

    working localstorage on contact form

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,46 +1,21 @@
-var userName = document.getElementById("contact-name").value;
-var userEmail = document.getElementById("contact-email").value;
-var userMessage = document.getElementById("user-message").value;
-var userRating = document.getElementById("user-rating").value;
+// STORES VALUES ON BUTTON CLICK
+function store() {
+    var userName = document.getElementById("user-name");
+    localStorage.setItem("user-name", userName.value);
 
-// WHEN USER CLICKS SAVE, THE TEXT VALUE IS OBTAINED
-$("#submit-button").on("click", function () {
-    // OBTAIN TEXT VALUES FROM ROW CLASS AND ID 
-    // var textValue = $(this).siblings(".description").val();
+    var userEmail = document.getElementById("user-email");
+    localStorage.setItem("user-email", userEmail.value);
 
-    // SET TO SAVE IN LOCALSTORAGE
-    localStorage.setItem("user-name" textValue);
-    localStorage.setItem("user-email", textValue);
-    localStorage.setItem("user-message", textValue);
-    localStorage.setItem("user-rating", textValue);
-});
+    var userMessage = document.getElementById("user-message");
+    localStorage.setItem("user-message", userMessage.value);
 
-// Set Name to localStorage
+    var userRating = document.getElementById("user-rating");
+    localStorage.setItem("user-rating", userRating.value);
+}
 
-$("name .description").val(localStorage.getItem(userName));
+// GET ITEM 
 
-// Set Email to localStorage
-
-$("#email .description").val(localStorage.getItem(userEmail));
-
-// Set Message to localStorage 
-
-$("#message .description").val(localStorage.getItem(userMessage));
-
-
-// Set Rating to localStorage (?)
-
-$("#rating .description").val(localStorage.getItem(userRating));
-
-// var saveTasks = function() {
-//     localStorage.setItem("tasks", JSON.stringify(tasks));
-//   };
-
-//   // remove all tasks
-// $("#remove-tasks").on("click", function() {
-//     for (var key in tasks) {
-//       tasks[key].length = 0;
-//       $("#list-" + key).empty();
-//     }
-//     saveTasks();
-//   });
+// var storedName = localStorage.getItem("user-name");
+// var storedEmail = localStorage.getItem("user-email");
+// var storedMessage = localStorage.getItem("user-message");
+// var storedRating = localStorage.getItem("user-rating");
