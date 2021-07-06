7c60f658ac2d8279f63d8ad3ee1dc3c0daf2cd09
AniElaina <anicook13@gmail.com>
2021-07-06 11:36:14 -0400

    pushing styled Recently Viewed Page with updated css, js, and html links

--- assets/js/script1.js
+++ assets/js/script1.js
@@ -1,16 +1,4 @@
 
-
-//create variables to better define recipe title & image...Do I need to do this or can I pull from previous function??
-// my goal is to display previously viewed recipes (based on user ingredient search) to a seperate page in seperate JS file
-//first declare an object which stores previous recipes
-// var savedRecipes = {};
-// everytime the website loads for the first time, check to see if there is history object
-
-// in the localstorage, and if there is parse it, and set it as the initial value of the history object
-// if there isn't set history object to be empty {}
-// if (!savedRecipes) {
-//     savedRecipes = {};
-// }
 // when we look for recipes store a key -value pair in our history object
 // object key = {title} value = {link}
 // localstorage key history = value {stringified object}
@@ -22,63 +10,53 @@
 // parse it
 // and use all key value pairs, to display cards, with keys as title and vaues as src attribute to images
 
-///////////////////////LAST THREE RECIPES FROM HOME PAGE ARE BEING STORED IN THE CONSOLE IN USERPICK PAGE, I JUST NEED TO PUSH THEM TO DISPLAY ON THE PAGE////////////////////
-var arrayImageEl = document.getElementById("card-3");
-var arrayPrintEl = document.getElementById("arrayPrint");
-var colOneEl = document.getElementById("colOne");
-var colTwoEl = document.getElementById("colTwo");
-var colThreeEl = document.getElementById("colThree");
+///////////////////////First and Last Images are being displayed to page. Leaving col and img variables for now in case of format change/////////////////////////
+// var arrayImageEl = document.getElementById("card-3");
+// var arrayPrintEl = document.getElementById("arrayPrint");
+// var colOneEl = document.getElementById("colOne");
+// var colTwoEl = document.getElementById("colTwo");
+// var colThreeEl = document.getElementById("colThree");
+var cardOneEl = document.getElementById("cardOne");
+var cardTwoEl = document.getElementById("cardTwo");
+var cardThreeEl = document.getElementById("cardThree");
+// var imgOneEl = document.getElementById("imgOne");
+// var imageTwoEl = document.getElementById("imgTwo");
+// var imageThreeEl = document.getElementById("imgThree");
 
 var displayLastThreeRecipes = function () {
     var recipesArray = localStorage.getItem("lastThreeRecipes");
     var lastThreeRecipes = JSON.parse(recipesArray);
     console.log(lastThreeRecipes);
-    console.log(lastThreeRecipes[0].id); /// use this format in for loop to grab other parts of array 
+    console.log(lastThreeRecipes[0].id); /// use this format in for loop to grab parts of array 
     for (let i = 0; i < lastThreeRecipes.length; i++) {
-        colTwoEl.innerHTML = lastThreeRecipes[0].title; /// displays title   ***********************
+        cardOneEl.innerHTML = lastThreeRecipes[0].title; /// displays title 1  ***********************
+        cardTwoEl.innerHTML = lastThreeRecipes[1].title; /// displays title 2 ************************
+        cardThreeEl.innerHTML = lastThreeRecipes[2].title; /// displays title 3 **********************
         // arrayPrintEl.appendChild.lastThreeRecipes[1].title;
         // document.getElementById("arrayPrint").innerHTML = JSON.stringify(recipesArray); // displays array as is
         var image = document.createElement("img");
         image.setAttribute('src', " ");
         image.setAttribute('src', lastThreeRecipes[0].image);
-        colTwoEl.appendChild(image);       ///// first image is displayed ***************************
-        colOneEl.appendChild(lastThreeRecipes[0].title)
+        cardOneEl.appendChild(image);       ///// first image is displayed ***************************
+        // cardOneEl.appendChild(lastThreeRecipes[0].title)
 
 
         var image1 = document.createElement("img");
         image1.setAttribute('src', " ");
         image1.setAttribute('src', lastThreeRecipes[1].image1);
-        // colTwoEl.appendChild(image1);         /////// second image is not displayed......
-        // colTwoEl.innerHTML = lastThreeRecipes[1].title;
+        cardTwoEl.appendChild(image1);         /////// second image is not displayed......
+
 
         var image2 = document.createElement("img");
         image2.setAttribute('src', " ");
         image2.setAttribute('src', lastThreeRecipes[2].image);   ///// third image is displayed *********************************
-        // colThreeEl.appendChild(image2);
-        colThreeEl.innerHTML = lastThreeRecipes[2].title;   //// the title makes the image dissappear? ? 
+        cardThreeEl.appendChild(image2);
+        // cardThreeEl.innerHTML = lastThreeRecipes[2].title;   
 
 
 
-        // var newDiv = document.createElement("div");
-        // newDiv.innerHTML = lastThreeRecipes[1].title;
-        // newDiv.appendChild(recipesArray[i]);
         console.log(lastThreeRecipes);
-        /////////////onto something here/////////////////////////////////////////////
-        ///////////// maybe empty out innerHTML then try again///////////////////
-        // document.getElementById("arrayPrint").innerHTML = "";
-        var recipeTitle1 = lastThreeRecipes[1].title;
-        console.log(recipeTitle);   /////////successfully console logged first recipe title
 
-        newDiv.appendChild(recipeTitle1);  /// when i user innerHTML here instead of append, I get three responses back to console. 
-        newDiv.innerHTML = lastThreeRecipes[1].title;
-
-        lastThreeRecipes.find(x => x.b === 3)
-
-
-        // var titleEl = document.createElement("span");
-        // recipeTitle.textContent = lastThreeRecipes.title;
-
-        // titleEl.append(lastThreeRecipes.textContent);  
     }
 }
 displayLastThreeRecipes();
@@ -92,8 +70,8 @@ displayLastThreeRecipes();
 // imgData = getBase64Image(recipeImage);
 // localStorage.setItem("lastThreeRecipes", imgData);
 
-// function getBase64Image(img) {            ////// LOCAL STORAGE ONLY SUPPORTS STRINGS (APPROX 5MB) SO MUST USE BASE64 DATA THEN CONVERT USING CANVAS
-//     var canvas = document.createElement("canvas");
+// function getBase64Image(img) {            ////// LOCAL STORAGE ONLY SUPPORTS STRINGS (& APPROX 5MB) SO MUST USE BASE64 DATA THEN CONVERT USING CANVAS
+//     var canvas = document.createElement("canvas"); /// NVM BC THANKFULLY SPOONACULAR HAS LOW QUALITY PHOTOS!!
 //     canvas.width = img.width;
 //     canvas.height = img.height;
 
