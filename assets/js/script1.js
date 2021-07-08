38a9cc59885692e50027c6158600b30d4fd00973
AniElaina <anicook13@gmail.com>
2021-07-07 23:11:46 -0400

    updated RecentlyViewed Page

--- assets/js/script1.js
+++ assets/js/script1.js
@@ -1,4 +1,4 @@
-
+// PSEUDOCODING////
 // when we look for recipes store a key -value pair in our history object
 // object key = {title} value = {link}
 // localstorage key history = value {stringified object}
@@ -10,12 +10,8 @@
 // parse it
 // and use all key value pairs, to display cards, with keys as title and vaues as src attribute to images
 
-///////////////////////First and Last Images are being displayed to page. Leaving col and img variables for now in case of format change/////////////////////////
-// var arrayImageEl = document.getElementById("card-3");
-// var arrayPrintEl = document.getElementById("arrayPrint");
-// var colOneEl = document.getElementById("colOne");
-// var colTwoEl = document.getElementById("colTwo");
-// var colThreeEl = document.getElementById("colThree");
+///////////////////////First and Last Images are displayed to page/////////////////////////
+
 var cardOneEl = document.getElementById("cardOne");
 var cardTwoEl = document.getElementById("cardTwo");
 var cardThreeEl = document.getElementById("cardThree");
@@ -61,63 +57,7 @@ var displayLastThreeRecipes = function () {
 }
 displayLastThreeRecipes();
 
-// var displayRecipeOne = function () {
-//     var firstRecipe = localStorage.getItem("recipeTitle");
-//     var recipeOne = JSON.parse(firstRecipe);
-//     console.log(recipeOne);
-// }
-// recipeImageEl = document.getElementById('recipeImage');
-// imgData = getBase64Image(recipeImage);
-// localStorage.setItem("lastThreeRecipes", imgData);
 
-// function getBase64Image(img) {            ////// LOCAL STORAGE ONLY SUPPORTS STRINGS (& APPROX 5MB) SO MUST USE BASE64 DATA THEN CONVERT USING CANVAS
-//     var canvas = document.createElement("canvas"); /// NVM BC THANKFULLY SPOONACULAR HAS LOW QUALITY
-//     canvas.width = img.width;
-//     canvas.height = img.height;
-
-//     var ctx = canvas.getContext("2d");
-//     ctx.drawImage(img, 0, 0);
-
-//     var dataURL = canvas.toDataURL("image/png");
-
-//     return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
-// }
-
-// var dataImage = localStorage.getItem('imgData');
-// recipeImageEl = document.getElementById('recipeImage');
-// recipeImage.src = "data:image/png;base64," + dataImage;
-
-
-
-
-
-// document.appendChild(lastThreeRecipes[0].id)
-
-
-
-
-// body.append(recipeTitle.lastThreeRecipes[0].image)
-
-// document.getElementById("arrayPrint").innerHTML = JSON.stringify(recipesArray[0].image.); // may be better 
-// var newDiv = document.createElement("div");
-// newDiv.innerHTML = lastThreeRecipes.title
-// newDiv.appendChild(recipesArray[i]);
-// img.append(recipesArray[i].image[0]);
-// img.append(recipesArray[i].image[1]);
-// img.append(recipesArray[i].image[2]);
-
-
-
-
-
-// lastThreeRecipes = response.results.slice(0, 3);
-// localStorage.setItem("lastThreeRecipes", JSON.stringify(lastThreeRecipes));
-// console.log(lastThreeRecipes);
-// new.appendChild(lastThreeRecipes);
-
-
-
-/////////////////////////////////////////////////////////////////////////////////////////////
 
 
 
